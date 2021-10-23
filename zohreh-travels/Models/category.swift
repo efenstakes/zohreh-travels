4c40adaa6c144c95d02e2fd10569192ed39bd663
Efen Stakes <efenstakes101@gmail.com>
2021-10-23 16:28:19 +0300

    add app images

---
+++ zohreh-travels/Models/category.swift
@@ -0,0 +1,8 @@
+//
+//  category.swift
+//  zohreh-travels
+//
+//  Created by MAC on 10/23/21.
+//
+
+import Foundation
---
+++ zohreh-travels/Models/product.swift
@@ -0,0 +1,8 @@
+//
+//  product.swift
+//  zohreh-travels
+//
+//  Created by MAC on 10/23/21.
+//
+
+import Foundation
