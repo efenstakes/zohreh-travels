5dff2f264b5f497602aee6f3d2126e74451fe407
kipchumba26 <cynthia.kipchumba@nativecoders.se>
2021-10-23 14:29:55 +0300

    Initial Commit

---
+++ zohreh-travels/zohreh_travelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  zohreh_travelsApp.swift
+//  zohreh-travels
+//
+//  Created by MAC on 10/23/21.
+//
+
+import SwiftUI
+
+@main
+struct zohreh_travelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
